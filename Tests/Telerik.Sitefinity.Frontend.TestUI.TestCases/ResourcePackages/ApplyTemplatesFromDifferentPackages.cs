21b07225b51f21f9174b873da2b4531bc8c6e20d
vassildinev <vassildinev@gmail.com>
2016-05-10 16:58:03 +0300

    Increased post-setup timeout

--- Tests/Telerik.Sitefinity.Frontend.TestUI.TestCases/ResourcePackages/ApplyTemplatesFromDifferentPackages.cs
+++ Tests/Telerik.Sitefinity.Frontend.TestUI.TestCases/ResourcePackages/ApplyTemplatesFromDifferentPackages.cs
@@ -49,7 +49,7 @@ namespace Telerik.Sitefinity.Frontend.TestUI.TestCases.ResourcePackages
         {
             RuntimeSettingsModificator.ExecuteWithClientTimeout(200000, () => BAT.Macros().User().EnsureAdminLoggedIn());
             BAT.Arrange(this.TestName).ExecuteSetUp();
-            RuntimeSettingsModificator.ExecuteWithClientTimeout(200000, () => BAT.Macros().User().EnsureAdminLoggedIn());
+            RuntimeSettingsModificator.ExecuteWithClientTimeout(300000, () => BAT.Macros().NavigateTo().Dashboard());
         }
 
         /// <summary>
